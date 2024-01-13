67620bfeb2076a747f6b6c2dd8b5fcee46cb65c1
WorkAccVikas <workaccvikas@gmail.com>
2024-01-13 15:35:18 +0530

    c8

---
+++ chai-backend/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // DESC : Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been successfully uploaded.
+    console.log("File is uploaded successfully (response) : ", response);
+
+    // DESC : After successfully upload file on cloudinary then we want to remove file from local server in sync
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    // DESC : Remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+  }
+};
+
+export { uploadOnCloudinary };
