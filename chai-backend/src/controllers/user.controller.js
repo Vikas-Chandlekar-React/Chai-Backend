d057456005fa5fe10b160b608b157797b2cfb950
WorkAccVikas <workaccvikas@gmail.com>
2024-01-15 17:00:22 +0530

    c11

--- chai-backend/src/controllers/user.controller.js
+++ chai-backend/src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+
+    // LEARN : It turn off validation rule because we want to save only few fields and does not run validation rule
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   /** DESC : Step for register
    *  1. get user details from frontend
@@ -82,4 +102,117 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  /** DESC : Step for login
+   *  1. get data from frontend in req.body
+   *  2. login using username or email (both way)
+   *  3. find the user in db
+   *  4. password check
+   *  5. generate access token and refresh token
+   *  6. send data as cookie
+   */
+
+  const { username, email, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "Username or Email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exists");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // LEARN : Sending secure cookie (we can't modified at frontend)
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  // LEARN : Send multiple cookies
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User Logged In Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  try {
+    /** PROBLEM :: (1) :
+     *  Here, We are not passing _id of user, so how to find the particular user who want to logout.
+     *  If we accepting _id so anyone can logout the user.
+     */
+    
+    //  SOLUTION :: (1) : Create auth middleware
+
+    /** DESC :
+     * Step for logout
+     *  1. get _id from req.user which is passed by auth middleware
+     *  2. set refreshToken to undefined or unset refreshToken (it will remove the refresh token field form document)
+     *  3. clear cookie from frontend side
+     */
+
+    // POINT : 1st way (using $set)
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          refreshToken: undefined,
+        },
+      },
+      { new: true }
+    );
+
+    // POINT : 2nd way (using $unset)
+    // await User.findByIdAndUpdate(
+    //   req.user._id,
+    //   {
+    //     $unset: {
+    //       refreshToken: 1, // this removes the field from document
+    //     },
+    //   },
+    //   { new: true }
+    // );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "User Logged Out Successfully"));
+  } catch (error) {}
+});
+
+export { registerUser, loginUser, logoutUser };
